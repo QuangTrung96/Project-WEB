db74560cdf2e01cf742107d0dac133e88780b9a1
QuangTrung <trinhquangtrung1b@gmail.com>
2018-11-30 17:18:42 +0700

    Update code 5:18

--- Project/resources/views/hus/index.blade.php
+++ Project/resources/views/hus/index.blade.php
@@ -1,4 +1,59 @@
 @extends('master')
+<link href='{{ asset('public/css/bootstrap.min.css') }}' rel='stylesheet' />
 @section('content')
-TQT
+<div class="container">
+  <h2>Carousel Example</h2>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel" width='80%'>
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="{{ asset('public/img/1.jpg') }}" alt="Los Angeles" style="width:60%;">
+        <div class="carousel-caption">
+          <h3>Los Angeles</h3>
+          <p>LA is always so much fun!</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="{{ asset('public/img/2.jpg') }}" alt="Chicago" style="width:60%;">
+        <div class="carousel-caption">
+          <h3>Chicago</h3>
+          <p>Thank you, Chicago!</p>
+        </div>
+      </div>
+    
+      <div class="item">
+        <img src="{{ asset('public/img/4.jpg') }}" alt="New York" style="width:60%;">
+        <div class="carousel-caption">
+          <h3>New York</h3>
+          <p>We love the Big Apple!</p>
+        </div>
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+   
+    	<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+			<span class="sr-only">Next</span>
+		</a>
+   
+  </div>
+</div>
+@endsection
+@section('body_scripts_bottom')
+<script src='{{ asset('public/js/bootstrap.min.js') }}'></script>
 @endsection
