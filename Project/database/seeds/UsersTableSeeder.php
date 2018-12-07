0cda0b5fd67e99c4d49a1190dc0a8ee31f0b4857
QuangTrung <trinhquangtrung1b@gmail.com>
2018-12-07 16:37:52 +0700

    Update code 16:40

--- Project/database/seeds/UsersTableSeeder.php
+++ Project/database/seeds/UsersTableSeeder.php
@@ -17,31 +17,31 @@ class UsersTableSeeder extends Seeder
                 'email'    => 'trinhquangtrung_t59@hus.edu.vn',
                 'password' => '123456',
                 'last_name' => 'Trịnh Quang',
-				'first_name' => 'Trung',
-				'permissions' => [
-					'admin' => true
-				]
+                'first_name' => 'Trung',
+                'permissions' => [
+                    'admin' => true
+                ]
             ],
             [
                 'username' => 'huynq6953',
                 'email'    => 'trinhquangtrung1b@gmail.com',
                 'password' => '123456',
-				'last_name' => 'Nguyễn Quang',
-				'first_name' => 'Huy',
-				'permissions' => [
+                'last_name' => 'Nguyễn Quang',
+                'first_name' => 'Huy',
+                'permissions' => [
 
-				]
+                ]
             ],
             [
-            	'username' => 'admin',
-				'email'    => 'admin@gmail.com',
-				'password' => '123456',
-				'last_name' => 'Trịnh Công',
-				'first_name' => 'Sơn',
-				'permissions' => [
-					'admin' => true
-				]
-		    ]
+                'username' => 'admin',
+                'email'    => 'admin@gmail.com',
+                'password' => '123456',
+                'last_name' => 'Trịnh Công',
+                'first_name' => 'Sơn',
+                'permissions' => [
+                    'admin' => true
+                ]
+            ]
         ];
 
         foreach ($data as $user) {
